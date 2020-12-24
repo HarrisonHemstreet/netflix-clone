bf5d09bbd070093752cbdf6c54549e2d015f3d29
Harrison <harrisonhemstreet@gmail.com>
2020-12-24 14:01:28 -0700

    added requests object

---
+++ src/requests.js
@@ -0,0 +1,16 @@
+const APIKEY = '3c4925d6c5bbd90119f13bee4186442e'
+
+//API End Points
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`
+    
+}
+
+export default requests;
