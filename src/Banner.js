5611e7e0bc57ce1fe09f4dfef3641f0458ca68db
Harrison <harrisonhemstreet@gmail.com>
2020-12-24 16:06:52 -0700

    banner guts done

--- src/Banner.js
+++ src/Banner.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import axios from './axios';
 import requests from './requests';
+import './Banner.css'
 
 function Banner() {
     const [movie, setMovie] = useState([]);
@@ -35,6 +36,12 @@ function Banner() {
                 <h1>
                     {movie?.title || movie?.name || movie?.original_name}
                 </h1>
+
+                <div className='banner__buttons'>
+                    <button className='banner__button'>Play</button>
+                    <button className='banner__button'>My List</button>
+                </div>
+                <h1 className='banner__description'>{movie?.overview}</h1>
             </div>
             {/* background img for the header */}
             {/* title */}
