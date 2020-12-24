f32f1709e75e195eb262e887d31f7ee3a56ee894
Harrison <harrisonhemstreet@gmail.com>
2020-12-24 15:33:22 -0700

    improved css on Row componenet

--- src/App.js
+++ src/App.js
@@ -5,7 +5,13 @@ import requests from './requests'
 function App() {
   return (
     <div className="App">
-      <Row title='NETFLIX ORIGINALS' fetchUrl={requests.fetchNetflixOriginals}/>
+      <Row
+        title='NETFLIX ORIGINALS' 
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+
+
       <Row title= 'Trending Now' fetchUrl={requests.fetchTrending}/>
       <Row title= 'Top Rated' fetchUrl={requests.fetchTopRated}/>
       <Row title= 'Action Movies' fetchUrl={requests.fetchActionMovies}/>
--- src/Row.js
+++ src/Row.js
@@ -4,7 +4,7 @@ import './Row.css'
 
 const base_url = 'https://image.tmdb.org/t/p/original/';
 
-function Row({title, fetchUrl}) {
+function Row({title, fetchUrl, isLargeRow}) {
     const [movies, setMovies] = useState([]);
 
     // runs based on a specific condition
@@ -28,8 +28,8 @@ function Row({title, fetchUrl}) {
                 {movies.map((movie) => (
                     <img
                         key={movie.id}
-                        className='row__poster'
-                        src={`${base_url}${movie.poster_path}`} 
+                        className={`row__poster ${isLargeRow && 'row__posterLarge'}`}
+                        src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} 
                         alt={movie.name}/>
                 ))}
             </div>
