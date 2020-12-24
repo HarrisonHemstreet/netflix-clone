b19d8dcd25cb4cb2681305fb0f38697aea40b7f7
Harrison <harrisonhemstreet@gmail.com>
2020-12-24 15:14:47 -0700

    fixed images

--- src/Row.js
+++ src/Row.js
@@ -25,8 +25,12 @@ function Row({title, fetchUrl}) {
             <div className='row__posters'>
                 {/* several row__poster(s) */}
 
-                {movies.map(movie => (
-                    <img src={`${base_url}${movie.poster_path}`} alt={movie.name}/>
+                {movies.map((movie) => (
+                    <img
+                        key={movie.id}
+                        className='row__poster'
+                        src={`${base_url}${movie.poster_path}`} 
+                        alt={movie.name}/>
                 ))}
             </div>
             {/* container --> posters */}
