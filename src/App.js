cf61431bd3609b8823163138942676056637b4fc
Harrison <harrisonhemstreet@gmail.com>
2020-12-24 14:27:31 -0700

    created hooks in Row.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Row from './row'
+import requests from './requests'
 
 function App() {
   return (
     <div className="App">
-      
+      <Row title='NETFLIX ORIGINALS' fetchUrl={requests.fetchNetflixOriginals}/>
+      <Row title= 'Trending Now' fetchUrl={requests.fetchTrending}/>
+      <Row title= 'Top Rated' fetchUrl={requests.fetchTopRated}/>
+      <Row title= 'Action Movies' fetchUrl={requests.fetchActionMovies}/>
+      <Row title= 'Comedies' fetchUrl={requests.fetchComedyMovies}/>
+      <Row title= 'Horror' fetchUrl={requests.fetchHorrorMovies}/>
+      <Row title= 'Romance' fetchUrl={requests.fetchRomanceMovies}/>
+      <Row title= 'Documentaries' fetchUrl={requests.fetchDocumentaries}/>
     </div>
   );
 }
