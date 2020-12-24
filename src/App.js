666fb795d27ba8f5d48a94f44b17ac2ceeac079b
Harrison <harrisonhemstreet@gmail.com>
2020-12-24 15:58:07 -0700

    added banner image and jazz

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import './App.css';
 import Row from './Row'
 import requests from './requests'
+import Banner from './Banner'
 
 function App() {
   return (
     <div className="App">
+      {/* Nav */}
+
+      <Banner />
       <Row
         title='NETFLIX ORIGINALS' 
         fetchUrl={requests.fetchNetflixOriginals}
         isLargeRow
       />
-
-
       <Row title= 'Trending Now' fetchUrl={requests.fetchTrending}/>
       <Row title= 'Top Rated' fetchUrl={requests.fetchTopRated}/>
       <Row title= 'Action Movies' fetchUrl={requests.fetchActionMovies}/>
