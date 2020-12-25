c47af0538886ca18e994194d99936e0c8b29bf6b
Harrison <harrisonhemstreet@gmail.com>
2020-12-25 09:06:06 -0700

    fades the banner image bottom

--- src/Banner.js
+++ src/Banner.js
@@ -45,8 +45,10 @@ function Banner() {
                     <button className='banner__button'>Play</button>
                     <button className='banner__button'>My List</button>
                 </div>
-                <h1 className='banner__description'>{movie?.overview}</h1>
+                <h1 className='banner__description'>
+                    {truncate(movie?.overview, 150)}</h1>
             </div>
+            <div className='banner--fadeBottom' />
             {/* background img for the header */}
             {/* title */}
             {/* div 2 buttons, play and my list */}
