19d5027e6e972d4af2c2666ba593b6786182fd3f
Harrison <harrisonhemstreet@gmail.com>
2020-12-25 09:13:40 -0700

    navbar pre flexbox

---
+++ src/Nav.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Nav() {
+    return (
+        <div className='nav'>
+            <img
+                className='nav__logo'
+                src='https://upload.wikimedia.org/wikipedia/commons/0/0f'
+                alt='Netflix Logo'
+            />
+            <img 
+                className='nav__avatar'
+                src='https://pbs.twimg.com/profile_images/'
+                alt='Netflix Logo'
+            />
+        </div>
+    )
+}
+
+export default Nav
